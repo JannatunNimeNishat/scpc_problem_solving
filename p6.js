3fbaa5e7936dee69df11f576e0b77f4012e2261a
JannatunNimeNishat <nishatnime100@gmail.com>
2023-07-27 15:03:29 +0600

    random password generator added

--- p6.js
+++ p6.js
@@ -1,1 +1,19 @@
 //Task 6: Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
+
+const passwordGenerator = (length) => {
+    const uppercaseLetters = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    const lowercaseChars = `abcdefghijklmnopqrstuvwxyz`;
+    const number = `0123456789`;
+    const specialChars = '!@#$%^&*()-_+=~`[]{}|:;"<>,.?/';
+    const allPossibleChar = uppercaseLetters + lowercaseChars + number + specialChars;
+    let finalPassword = '';
+
+    for(let i = 0; i<length;i++){
+        const randomIndex = Math.floor(Math.random() * allPossibleChar.length)
+        finalPassword += allPossibleChar[randomIndex]
+    }
+    return finalPassword
+}
+
+
+console.log(passwordGenerator(12));
